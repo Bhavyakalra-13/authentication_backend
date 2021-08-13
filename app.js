c5ec3dc759db94e789cf86aa16954959209eded4
Bhavya kalra <kalrabhavya13@gmail.com>
2021-08-14 05:19:36 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-import bodyParser from 'body-parser';
 const authenticationroutes = require("./routes/auth.js");
 const cors = require('cors')
 
@@ -9,16 +8,7 @@ const cors = require('cors')
 // http://localhost:8080/signup
 // http://localhost:8080/signin
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(cors({origin: '*'})),
 
 // db connection
 mongoose
